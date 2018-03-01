9df49ed49a16fb0ac3fea7147f35bce394e26ab2
feverdestiny <feverdestiny@163.com>
2018-03-01 13:32:07 +0800

    添加react-router-dom

---
+++ src/App.1.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import "./App.css";
+import { DatePicker } from "antd";
+
+import "antd/dist/antd.css";
+
+/**
+ * 定时器执行
+ *
+ * @class Click
+ * @extends {React.Component}
+ */
+class Click extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  componentWillMount() {
+    this.timeId = setInterval(() => {
+      this.tick();
+    }, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timeId);
+  }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  render() {
+    return <h1>hello,{this.state.date.toLocaleTimeString()}</h1>;
+  }
+}
+
+/**
+ * 开关切换
+ * 
+ * @class Toggle
+ * @extends {React.Component}
+ */
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggle: false
+    };
+    this.handerClick = this.handerClick.bind(this);
+  }
+  handerClick() {
+    this.setState(prevState => ({
+      isToggle: !prevState.isToggle
+    }));
+  }
+  render() {
+    return (
+      <button onClick={this.handerClick}>
+        {this.state.isToggle ? "NO" : "OFF"}
+      </button>
+    );
+  }
+}
+/**
+ * 列表循环
+ * 
+ * @class NumberList
+ * @extends {React.Component}
+ */
+class NumberList extends React.Component {
+  constructor(props) {
+    super(props);
+    let arr = [1, 2, 3, 4, 5, 6];
+    let list = arr.map(item => <li key={item.toString()}>hello,{item}</li>);
+    console.log(list);
+    this.state = {
+      list: list
+    };
+  }
+
+  render() {
+    // return <ul>{this.state.list}</ul>;
+    return <DatePicker />;
+  }
+}
+
+let element = (
+  <div>
+    <NumberList />
+  </div>
+);
+class App extends Component {
+  render() {
+    return element;
+  }
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,85 +1,72 @@
-import React, { Component } from "react";
-import "./App.css";
-import { DatePicker } from "antd";
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-import "antd/dist/antd.css";
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
+      </ul>
 
-/**
- * 定时器执行
- *
- * @class Click
- * @extends {React.Component}
- */
-class Click extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() };
-  }
-  componentWillMount() {
-    this.timeId = setInterval(() => {
-      this.tick();
-    }, 1000);
-  }
-  componentWillUnmount() {
-    clearInterval(this.timeId);
-  }
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-  render() {
-    return <h1>hello,{this.state.date.toLocaleTimeString()}</h1>;
-  }
-}
-class Toggle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isToggle: false
-    };
-    this.handerClick = this.handerClick.bind(this);
-  }
-  handerClick() {
-    this.setState(prevState => ({
-      isToggle: !prevState.isToggle
-    }));
-  }
-  render() {
-    return (
-      <button onClick={this.handerClick}>
-        {this.state.isToggle ? "NO" : "OFF"}
-      </button>
-    );
-  }
-}
+      <hr/>
 
-class NumberList extends React.Component {
-  constructor(props) {
-    super(props);
-    let arr = [1, 2, 3, 4, 5, 6];
-    let list = arr.map(item => <li key={item.toString()}>hello,{item}</li>);
-    console.log(list);
-    this.state = {
-      list: list
-    };
-  }
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+    </div>
+  </Router>
+)
 
-  render() {
-    // return <ul>{this.state.list}</ul>;
-    return <DatePicker />;
-  }
-}
-
-let element = (
+const Home = () => (
   <div>
-    <NumberList />
+    <h2>Home</h2>
   </div>
-);
-class App extends Component {
-  render() {
-    return element;
-  }
-}
+)
 
-export default App;
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
+export default BasicExample
